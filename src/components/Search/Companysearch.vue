12ffaec96e33473697af27e3cc3cec8a18328c56
dreamSeekerYu <3024637102@qq.com>
2019-10-10 07:40:49 +0800

    使用render company的 渲染连级搜索

---
+++ src/components/Search/Companysearch.vue
@@ -0,0 +1,119 @@
+<script>
+export default {
+  props: {
+    opt: {
+      type: Object,
+      required: true
+    }
+  },
+  render(h) {
+    if (this.opt.type === 'input') {
+      return h('input', {
+        attrs: {
+          placeholder: this.opt.label,
+          id: this.opt.label
+        },
+        domProps: {
+          innerHTML: 'asdf'
+        }
+      })
+    }
+    if (this.opt.type === 'multiple') {
+      return h('select', {
+        attrs: {
+          placeholder: this.opt.label,
+          id: this.opt.label
+        },
+        domProps: {
+          innerHTML: 'asdfghjkl;'
+        }
+      })
+    }
+    if (this.opt.type === 'radio') {
+      return h('select', {
+        attrs: {
+          placeholder: this.opt.label,
+          id: this.opt.label
+        },
+        domProps: {
+          innerHTML: 'asdfasdfa'
+        }
+      })
+    }
+    if (this.opt.type === 'time') {
+      return h('input', {
+        domProps: {
+          innerHTML: 'asdfasdfa'
+        }
+      })
+    }
+  }
+}
+
+// {
+//   // 和`v-bind:class`一样的 API
+//   'class': {
+//     foo: true,
+//     bar: false
+//   },
+//   // 和`v-bind:style`一样的 API
+//   style: {
+//     color: 'red',
+//     fontSize: '14px'
+//   },
+//   // 正常的 HTML 特性
+//   attrs: {
+//     id: 'foo'
+//   },
+//   // 组件 props
+//   props: {
+//     myProp: 'bar'
+//   },
+//   // DOM 属性
+//   domProps: {
+//     innerHTML: 'baz'
+//   },
+//   // 事件监听器基于 `on`
+//   // 所以不再支持如 `v-on:keyup.enter` 修饰器
+//   // 需要手动匹配 keyCode。
+//   on: {
+//     click: this.clickHandler
+//   },
+//   // 仅对于组件，用于监听原生事件，而不是组件内部使用 `vm.$emit` 触发的事件。
+//   nativeOn: {
+//     click: this.nativeClickHandler
+//   },
+//   // 自定义指令。注意事项：不能对绑定的旧值设值
+//   // Vue 会为您持续追踪
+//   directives: [
+//     {
+//       name: 'my-custom-directive',
+//       value: '2',
+//       expression: '1 + 1',
+//       arg: 'foo',
+//       modifiers: {
+//         bar: true
+//       }
+//     }
+//   ],
+//   // Scoped slots in the form of
+//   // { name: props => VNode | Array<VNode> }
+//   scopedSlots: {
+//     default: props => createElement('span', props.text)
+//   },
+//   // 如果组件是其他组件的子组件，需为插槽指定名称
+//   slot: 'name-of-slot',
+//   // 其他特殊顶层属性
+//   key: 'myKey',
+//   ref: 'myRef'
+// }
+</script>
+<style lang="scss" scoped>
+input {
+  height: 30px;
+  line-height: 30px;
+}
+select {
+  width: 200px;
+}
+</style>
--- src/views/company/search.vue
+++ src/views/company/search.vue
@@ -1,9 +1,68 @@
 <template>
-  <div>search</div>
+  <div>
+    <label v-for="item in search_init" :key="item.key" :for="item.key">
+      <div>{{ item.label }}</div>
+      <Search :opt="item" />
+    </label>
+  </div>
 </template>
 
 <script>
-export default {}
+import Search from '@/components/Search/Companysearch.vue'
+const inviteStatus = []
+const intentLevel = []
+const boolOption = []
+
+const search_init = [
+  { label: '姓名', key: 'name', type: 'input' },
+  { label: '所在机构', key: 'institution', type: 'input' },
+  { label: '手机号', key: 'phone', type: 'input' },
+  { label: '职务', key: 'position', type: 'multiple' },
+  { label: '课程次数', key: 'courseNumber', type: 'number' },
+  { label: '是否Alpha用户', key: 'isAlphaUser', type: 'radio' },
+  { label: '是否iCourt学员', key: 'isIcourtStudent', type: 'radio' },
+  { label: '所在省份', key: 'province', type: 'multiple', list: {}},
+  { label: '所在城市', key: 'city', type: 'multiple', list: {}},
+  { label: '所在区县', key: 'area', type: 'multiple', list: {}},
+  { label: '标签', key: 'tags', type: 'multiple' },
+  { label: '邀请状态', key: 'inviteStatus', type: 'radio', list: inviteStatus },
+  { label: '邀请人', key: 'inviter', type: 'input' },
+  { label: '跟进记录', key: 'content', type: 'input' },
+  { label: '最后联系时间', key: 'lastContactTime', type: 'date' },
+  { label: '决策点', key: 'decisionPoint', type: 'input' },
+  { label: '意向程度', key: 'intentLevel', type: 'radio', list: intentLevel },
+  { label: '橙子手机', key: 'orangePhone', type: 'input' },
+  { label: '参加人数', key: 'joinActivityPersonNum', type: 'input' },
+  { label: '所在群', key: 'chatGroup', type: 'input' },
+  { label: '备注', key: 'remark', type: 'input' },
+  { label: '执业年限', key: 'workTime' },
+  { label: '出生日期', key: 'birthDay', type: 'birthDay' },
+  { label: '新增邀请日期', key: 'upDataTime', type: 'upData' },
+  {
+    label: '是否未来律所',
+    key: 'futureLawFlag',
+    type: 'radio',
+    list: boolOption
+  },
+  { label: '是否签到', key: 'sign', type: 'radio', list: boolOption },
+  { label: '未来律所活动', key: 'futureLaw', type: 'input' },
+  { label: '是否已购买该产品', key: 'isOrNot', type: 'radio', list: boolOption }
+]
+export default {
+  components: {
+    Search
+  },
+  data() {
+    return {
+      search_init
+    }
+  }
+}
+
+// render 实现组件的区分渲染
+// 文本   下拉选项(多选)    时间    双文本
+
+// http://localhost:8080/sales/api/v1/selections/configs
 </script>
 
 <style lang="scss" scoped>
